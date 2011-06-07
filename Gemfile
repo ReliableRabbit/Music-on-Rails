1daeec1a35a3a7b486433a565522b9b29882618d
Mohammad Typaldos <mohammad.elabid@gmail.com>
2011-06-07 17:29:45 -0400

    Init commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+# Created with
+# gem 'rails', '3.1.0.rc1'
+
+# Bundle edge Rails instead:
+gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3', :group => [:development, :test]
+gem 'pg', :group => :production
+
+# Client-side stuff
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicOnRails::Application.config.secret_token = '7be4095c89587406321e47fc2303c1d4d1ff6238413cb55cc728b13d77782314862a3f256339a0e52e8368194384e397ec15ceb6248db343613a02eccf321e9b'
